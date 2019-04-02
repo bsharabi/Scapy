cdc1eb4a31d3350176e95bb1f3a3f71d2b1bd4ab
Barak sharabi <41991103+bsharabi@users.noreply.github.com>
2019-04-02 12:15:17 +0300

    auto push

--- read_test.py
+++
@@ -1,9 +0,0 @@
-from scapy.all import *
-
-# rdpcap comes from scapy and loads in our pcap file
-packets = rdpcap('test.pcap')
-print(packets)
-
-pkt = packets[0]
-print(pkt.fields['subtype'])
-pkt.show()
--- send_test.py
+++
@@ -1,7 +0,0 @@
-from scapy.all import *
-
-# send a ICMP (ping) to local host - from google
-sr1(IP(dst="127.0.0.1",src="8.8.4.4")/ICMP())
-
-# send a DNS request to local hoste
-sr1(IP(dst="127.0.0.1")/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname="www.google.com")),verbose=0)
