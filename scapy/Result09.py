cd690a4b36b060e73f481e9d634a27bdd9443c15
Barak sharabi <41991103+bsharabi@users.noreply.github.com>
2019-04-09 02:00:16 +0300

    auto push

--- scapy/Result09.py
+++
@@ -1,5 +0,0 @@
-from scapy.all import *
-import re
-
-# rdpcap comes from scapy and loads in our pcap file
-packets = rdpcap('CaptureFile.pcap')
--- scapy/try.py
+++
@@ -1,4 +0,0 @@
-listCnt={"google":0,"ynet":0,"SuperPhramLogo.gif":0,"HelloWorld":0}
-for x in listCnt:
-    listCnt[x]+=1
-for x in listCnt:
