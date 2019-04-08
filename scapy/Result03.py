78e0ad73968f4e4f23ff59a120245b708125e4ad
Barak sharabi <41991103+bsharabi@users.noreply.github.com>
2019-04-09 02:01:16 +0300

    auto push

--- scapy/Result03.py
+++
@@ -1,9 +0,0 @@
-from scapy.all import *
-import re
-
-# rdpcap comes from scapy and loads in our pcap file
-packets = rdpcap('CaptureFile.pcap')
-
-print("------------------------- Result-03 -------------------------")
-print(len(packets))
-print("-------------------------------------------------------------")
--- scapy/send_test2.py
+++
@@ -1,8 +0,0 @@
-from scapy.all import *
-
-# send a ICMP (ping) to local host
-sr1(IP(dst="127.0.0.1",src="8.8.4.4")/ICMP())
-
-
-# send a ICMP (ping) to local host 
-sr1(IP(dst="127.0.0.1",src="1.1.1.1")/ICMP())
